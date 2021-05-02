9ef6777b1636e1e4d7410ca86ac162959946d2c6
sinshu <sinshu.d@gmail.com>
2021-05-02 21:47:51 +0900

    Add files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using NAudio.Wave;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var format = new WaveFormat(16000, 16, 1);
+        var silence = new SilenceProvider(format);
+
+        using (var waveOut = new WaveOut(WaveCallbackInfo.FunctionCallback()))
+        {
+            waveOut.Init(silence);
+            waveOut.Play();
+
+            Console.WriteLine("Press any key to stop.");
+            Console.ReadKey();
+        }
+    }
+}
